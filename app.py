d40b5c5eb0d43d9cf72889e46c3a0f141d64c7b1
itskshivam1318 <its.kshivam@gmail.com>
2020-03-14 13:23:36 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,117 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session, logging
+from flask_mysqldb import MySQL
+import logging
+import os
+import pickle
+from PIL import Image
+import numpy as np
+from matplotlib.pyplot import imshow
+from sklearn.preprocessing import OneHotEncoder
+import matplotlib.pyplot as plt
+from os import path
+import shutil
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+app.secret_key = "flash message"
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'stroke'
+
+mysql = MySQL(app)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/loginin', methods = ['GET','POST'])
+def loginin():
+    if request.method == "POST":
+        email = request.form['email_id']
+        password = request.form['password']
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT password from users where email = %s",(email,))
+        data = cur.fetchall()
+        if data[0][0] == password :
+            return redirect(url_for('home'))
+        else :
+            flash("Invalid Email_id or Password")
+            return redirect(url_for('login'))
+    return redirect(url_for('login'))
+
+
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+
+@app.route('/registration', methods=['POST'])
+def registration():
+    if request.method == "POST":
+        flash("User registered Successfully.")
+        fname = request.form['first_name']
+        lname = request.form['last_name']
+        email = request.form['email_id']
+        password = request.form['password']
+        gender = request.form['gender']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(fname,lname,email,password,gender) VALUES (%s,%s,%s,%s,%s)",
+                    (fname, lname, email, password, gender))
+        mysql.connection.commit()
+        return redirect(url_for("register"))
+
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def uploader():
+    if request.method == 'POST':
+        file = request.files['file']
+        file.save(secure_filename(file.filename))
+        print(file.filename)
+        if path.exists("input.jpg"):
+            os.remove("input.jpg")
+        os.rename(file.filename, r'input.jpg')
+        #shutil.move(file.filename,'/uploads')
+        flash("File uploaded successfully")
+    return redirect(url_for('output'))
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/output')
+def output():
+    with open('model_pickle', 'rb') as f:
+        mp = pickle.load(f)
+    enc = OneHotEncoder()
+    enc.fit([[0], [1]])
+    def names(number):
+        if (number == 0):
+            return 'Stroke'
+        else:
+            return 'Normal'
+    img = Image.open(r"input.jpg")
+    x = np.array(img.resize((128, 128)))
+    x = x.reshape(1, 128, 128, 3)
+    answ = mp.predict_on_batch(x)
+    classification = np.where(answ == np.amax(answ))[1][0]
+    imshow(img)
+    return str(answ[0][classification] * 100) + '% Confidence This Is ' + names(classification)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ stroke .sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 14, 2020 at 07:31 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.1.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `stroke`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(60) NOT NULL,
+  `lname` varchar(60) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `gender` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fname`, `lname`, `email`, `password`, `gender`) VALUES
+(1, 'shivam', 'kharje', 'shivamkharje@gmail.com', '12345678', 'male');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
